07dee85263655a630fc98ce7219293e4e6515759
liujun-dlink <jun.liu@cn.dlink.com>
2021-02-09 15:31:39 +0800

    bugfix

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports.ddsnapi = require("./build/Release/ddsnapi");
+// module.exports.ddsnapi = require("./build/Release/ddsnapi");
+const addon = require("./build/Release/ddsnapi");
+
+exports.getPerformance = function(callback){
+  addon.getPerformance(callback);
+}
+
+exports.getPerformanceSync = function(callback){
+  addon.getPerformanceSync(callback);
+}
